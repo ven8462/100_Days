06bbc54e4cb11f564370700fbfa63b6ce7646d21
lavender <lavenderanyango9@gmail.com>
2023-07-04 01:59:15 +0300

    Day_1

---
+++ python_projects/band-name-generator.py
@@ -0,0 +1,9 @@
+print("Hello, Welcome to the Band Name Generator")
+
+city = input("What is your favourite city\n")
+
+pet = input("What is your pets name?\n")
+
+band_name = city + pet
+
+print("Your band name could be: {}".format(band_name))
