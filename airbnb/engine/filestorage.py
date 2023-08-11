6d95ef47a18bed21d96079ce469b9abbe67024f1
lavender <lavenderanyango9@gmail.com>
2023-08-12 00:50:23 +0300

    password generator updated

---
+++ airbnb/engine/filestorage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+
+
--- python_projects/password_generator.py
+++ python_projects/password_generator.py
@@ -2,7 +2,7 @@
 
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'h',  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E','F', 'J', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'j', 'h',  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'J', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -10,7 +10,7 @@ symbols = ['~', '#', '$', '%', '*', '+']
 
 print("Welcome to Ven's password generator")
 
-nl = int(input("How many letters do you want?"))
+nl = int(input("How many letters do you want? "))
 nn = int(input("how many numbers do you want? "))
 
 ns = int(input("how many symbols do you want? "))
@@ -45,11 +45,11 @@ for n in range(1, nn + 1):
 for s in range(1, ns + 1):
     pas = random.choice(symbols)
     password_list += pas
-print(password_list)
+# print(password_list)
 
 random.shuffle(password_list)
-print(password_list)
-#password = "".join(password_list)
+# print(password_list)
+password = "".join(password_list)
 """for char in password_list:
     password += char"""
 print("your password_list is:  {}".format(password))
