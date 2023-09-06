9aac42c8273b18a6eb5906fa238e96d59abdd3ed
lavender <lavenderanyango9@gmail.com>
2023-09-06 15:20:58 +0300

    cipher code

--- python_projects/caesar_cipher.py
+++ python_projects/caesar_cipher.py
@@ -1,38 +1,87 @@
 #!/usr/bin/python3
 
+from sys import exit
+from art import logo
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
+def cipher(message, shift_no, cipher_direction):
+    """combining the encrypt and decrypt function in one function"""
+    cipher_text = ""
+    if cipher_direction == "decode":
+        shift_no *= -1
+    for chars in message:
+        if chars in alphabet:
+            position = alphabet.index(chars)
+            new_position = position + shift_no
+            new_chars = alphabet[new_position]
+            cipher_text += new_chars
+        else:
+            cipher_text += chars
+    print(f"the {direction}d message is {cipher_text}") 
 
 
 
-def encrypt(plain_text, shift):
-    encoded_text = ""
-    for letters in plain_text:
-        position = alphabet.index(letters)
-        new_position = position + shift
-        new_letter = alphabet[new_position]
-        encoded_text += new_letter
-
-    print("the encrypted text is",encoded_text)
 
 
+while True:
+    prompt = ['encode', 'decode']
 
-def decrypt(plain_text, shift):
-    decoded_text = ""
-    for letters in plain_text:
-        position = alphabet.index(letters)
-        new_position = position - shift
-        new_letter = alphabet[new_position]
-        decoded_text += new_letter
-    print("the decrypted code is", decoded_text)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    while direction not in prompt:
+        direction = input('Please enter the right keyword❗, type in "encode" to encrypt or type in "decode" to decrypt\n')
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    
+
+    shift = shift % 52
+    cipher(message=text, shift_no=shift, cipher_direction=direction)
 
 
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
-else:
-    print("please enter encode or decode")
+    rerun = input("\nHey!, Do you wish to rerun the program😊?: ")
+    if rerun.islower() == "no":
+        print("Thank you for using the Caesar Cipher program by Lavender❤️, Bye Bye😍😍")
+        break
+     #   exit(0)
+    # else:
+    #     cipher(message=text, shift_no=shift, cipher_direction=direction)
+
+
+
+
+
+
+
+
+
+# def encrypt(plain_text, shift):
+#     encoded_text = ""
+#     for letters in plain_text:
+#         position = alphabet.index(letters)
+#         new_position = position + shift
+#         new_letter = alphabet[new_position]
+#         encoded_text += new_letter
+
+#     print("the encrypted text is",encoded_text)
+
+
+
+# def decrypt(plain_text, shift):
+#     decoded_text = ""
+#     for letters in plain_text:
+#         position = alphabet.index(letters)
+#         new_position = position - shift
+#         new_letter = alphabet[new_position]
+#         decoded_text += new_letter
+#     print("the decrypted code is", decoded_text)
+
+
+# if direction == "encode":
+#     encrypt(text, shift)
+# elif direction == "decode":
+#     decrypt(text, shift)
+# else:
+#     print("please enter encode or decode")
