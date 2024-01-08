2eeb237c22d166ccb6829916e9fb8df7480074a6
lavender <lavenderanyango9@gmail.com>
2024-01-08 11:22:23 +0300

    number methods

---
+++ HTML/js-practice/task2.js
@@ -0,0 +1,12 @@
+// working with variables
+
+let favFood = "Pizza";
+let favColor = "Red";
+let favDrink = "Lemonade";
+
+console.log(favDrink);
+favFood = "Pasta";
+console.log(favFood);
+
+let myBirthDay = "15th Oct 2000"
+console.log(myBirthDay);
---
+++ HTML/js-practice/task3.js
@@ -0,0 +1,14 @@
+// practising number methods
+
+let PI = 3.14159;
+
+let num = PI.toFixed(3);
+console.log(num);
+
+let num2 = PI.toPrecision(3);
+console.log(num2);
+
+
+let lightSpeed = 186000;
+let lightSpeed2 = lightSpeed.toExponential();
+console.log(lightSpeed2);
