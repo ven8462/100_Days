d8a4f674d0c9d3d25dac306a104f9e96da2a4cdb
lavender <lavenderanyango9@gmail.com>
2024-01-08 11:42:15 +0300

    string literals

---
+++ HTML/js-practice/task4.js
@@ -0,0 +1,9 @@
+// template literals
+
+let firstName = "Lavender";
+let course = "JavaScript";
+let channel = "Dev Dreamer";
+
+let info = `${firstName} is learning ${course} with ${channel}`;
+
+console.log(info);
